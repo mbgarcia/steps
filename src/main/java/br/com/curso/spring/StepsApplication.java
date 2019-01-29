5365353373a149e6d7936a76a8f6d0bbc810b9d7
Marcelo de Brito Garcia <mbgarcia.pa@gmail.com>
2019-01-29 16:26:21 -0300

    Start project

---
+++ src/main/java/br/com/curso/spring/StepsApplication.java
@@ -0,0 +1,14 @@
+package br.com.curso.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/curso/spring/StepsApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.curso.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StepsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
